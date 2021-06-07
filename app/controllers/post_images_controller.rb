5ddedd2dddb1f866b014d27be62d45e3b6a84ad0
EC2 Default User <ec2-user@ip-172-31-36-2.ec2.internal>
2021-06-07 07:23:28 +0000

    [ADD]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,10 +16,16 @@ def index
 end
 
 def show
+  @post_image = PostImage.find(params[:id])
 end
 
 def destroy
+  @post_image = PostImage.find(params[:id])
+  @post_image.destroy
+  redirect_to post_images_path
 end
+
+
 
 private
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '765613413318c53535d705df5581708196bbd333076e6f028b73e72bbda43dff63cb994b8b9428d7b7422bcea24dbdf183c1a8a118b66c1a41aef3f5be9387e3'
